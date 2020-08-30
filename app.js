1e21476e057a1ce3df0a3da059b874fe4245bc2b
Aman Kant Shakya <amankant19@gmail.com>
2020-08-30 22:23:35 +0530

    added server

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-const log =require('./logger');
-log('message');
+var http =require("http");
+
+http.createServer(function (request,response) {
+	response.writeHead(200,{'ContentType':'text/plain'})
+
+	response.end('Hello world\n');
+	// body...
+}).listen(8081);
+
+
+console.log('running');
