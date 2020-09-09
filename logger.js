cad746c6c20ffdd349593345db1cd6486b8d69d4
Aman Kant Shakya <amankant19@gmail.com>
2020-09-09 23:56:50 +0530

    updated logger

--- logger.js
+++ logger.js
@@ -1,13 +1,8 @@
-var url='http://log.io/';
-
-function log(message) {
-	
-	var number=10;
-	var num2=15;
-	var sum = number+num2;
-	console.log(sum);
-	console.log(message);
-	// body...
-}
-
-module.exports=log;
+function factorial(n){
+	if(n>=1)
+	   return n*factorial(n-1);
+	else
+	   return 1;
+   
+   } 
+   
